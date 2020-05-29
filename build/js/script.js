b83694a50bb45ac40ec0aa06670f6617650a5288
Кирилл <kirillmorgunov@mail.ru>
2020-05-29 12:59:49 +0300

    добавлены якоря и ссылки на них, сверстано выпадающее меню, добавлена логика Js для меню

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,25 @@
-'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+"use strict";
+var mainMenu = document.querySelector(".main-menu");
+var headerToggle = document.querySelector(".page-header__toggle");
+var closeButton = document.querySelector(".main-menu__close-button")
 
-pageHeader.classList.remove('page-header--nojs');
+mainMenu.classList.remove("main-menu--nojs");
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+headerToggle.addEventListener("click", function () {
+    mainMenu.classList.remove("main-menu--closed");
+    mainMenu.classList.add("main-menu--opened");
+});
+
+closeButton.addEventListener("click", function(){
+  mainMenu.classList.add("main-menu--closed");
+  mainMenu.classList.remove("main-menu--opened");
+})
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    if (mainMenu.classList.contains("main-menu--opened")) {
+      mainMenu.classList.remove("main-menu--opened");
+      mainMenu.classList.add("main-menu--closed");
+    }
   }
 });
