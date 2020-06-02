a115acddf81706183ce63130e6b94ae7afda6d2c
Кирилл <kirillmorgunov@mail.ru>
2020-06-02 14:54:35 +0300

    убрал outline у кнопки при нажатии

--- source/js/scripts/phoneMask.js
+++ source/js/scripts/phoneMask.js
@@ -1,5 +1,5 @@
-(function() {
-$(document).ready(function(){
-  $('.tel').mask('0-000-000-00-00');
-});
+(function () {
+  $(document).ready(function () {
+    $(".tel").mask("0-000-000-00-00");
+  });
 })();
--- source/js/scripts/script.js
+++ source/js/scripts/script.js
@@ -1,33 +1,33 @@
-(function() {
-"use strict";
-var mainMenu = document.querySelector(".main-menu");
-var headerToggle = document.querySelector(".page-header__toggle");
-var closeButton = document.querySelector(".main-menu__close-button")
+(function () {
+  "use strict";
+  var mainMenu = document.querySelector(".main-menu");
+  var headerToggle = document.querySelector(".page-header__toggle");
+  var closeButton = document.querySelector(".main-menu__close-button");
 
-if (mainMenu) {
-  mainMenu.classList.remove("main-menu--nojs");
-}
-
-if (headerToggle) {
-  headerToggle.addEventListener("click", function () {
-    mainMenu.classList.remove("main-menu--closed");
-    mainMenu.classList.add("main-menu--opened");
-});
-}
-
-if (closeButton) {
-  closeButton.addEventListener("click", function(){
-    mainMenu.classList.add("main-menu--closed");
-    mainMenu.classList.remove("main-menu--opened");
-  })
-}
-
-window.addEventListener("keydown", function(evt) {
-  if (evt.keyCode === 27) {
-    if (mainMenu.classList.contains("main-menu--opened")) {
-      mainMenu.classList.remove("main-menu--opened");
-      mainMenu.classList.add("main-menu--closed");
-    }
+  if (mainMenu) {
+    mainMenu.classList.remove("main-menu--nojs");
   }
-});
+
+  if (headerToggle) {
+    headerToggle.addEventListener("click", function () {
+      mainMenu.classList.remove("main-menu--closed");
+      mainMenu.classList.add("main-menu--opened");
+    });
+  }
+
+  if (closeButton) {
+    closeButton.addEventListener("click", function () {
+      mainMenu.classList.add("main-menu--closed");
+      mainMenu.classList.remove("main-menu--opened");
+    });
+  }
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (mainMenu.classList.contains("main-menu--opened")) {
+        mainMenu.classList.remove("main-menu--opened");
+        mainMenu.classList.add("main-menu--closed");
+      }
+    }
+  });
 })();
